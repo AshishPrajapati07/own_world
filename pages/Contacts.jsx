705760909e9b77ba62ca8e728da6e5541d0688f9
Ashish_Prajapati <ashish.spyhunter@gmail.com>
2022-08-01 16:53:46 +0530

    Add Contact Page

--- pages/Contacts.jsx
+++ pages/Contacts.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <Wrapper className="section">
+      <h2 className="common-heading">Contact us</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d232.43335283114106!2d72.90622935034845!3d21.234479683319123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be0475a8b4f9c41%3A0x73cbbce6635f7c29!2sSpyhunter%20IT%20Solution!5e0!3m2!1sen!2sin!4v1659341711869!5m2!1sen!2sin"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        // referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/mknewbwo"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              autoComplete="off"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+            <textarea
+              name="message"
+              cols="30"
+              rows="6"
+              autoComplete="off"
+              placeholder="Type Your Message"
+              required
+            ></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Contacts
+const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0;
+
+  .container {
+    margin-top: 6rem;
+    text-align: center;
+  }
+  .contact-form {
+    max-width: 50rem;
+    margin: auto;
+
+    .contact-inputs {
+      display: flex;
+      flex-direction: column;
+      gap: 3rem;
+
+      input[type="submit"] {
+        cursor: pointer;
+        transition: all 0.2s;
+        &:hover {
+          background-color: ${({ theme }) => theme.colors.white};
+          border: 1px solid ${({ theme }) => theme.colors.btn};
+          color: ${({ theme }) => theme.colors.btn};
+          transform: scale(0.9);
+        }
+      }
+    }
+  }
+`;
+
+export default Contacts;
